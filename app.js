63376bc4781ec81ee696fc76ac414f5fccf3e874
Seham-Hammad <seham.hammad@yahoo.ie>
2020-04-17 13:18:21 +0100

    hi

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const http = require('http');
+const _ = require('underscore');
 const server = http.createServer();
-server.on('Connection', (socket)=> {
-    console.log('New Connection...')
 
-});
 server.listen(3999);
 
 console.log('Listening on port 3999');
